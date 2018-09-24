3fbb588b4e12586fe5894436a094b07224d74bfc
LPK <k.lesle@live.com>
2018-09-24 21:27:54 +0530

    sdf

---
+++ src/app/student/student.ts
@@ -0,0 +1,14 @@
+export class Student{
+    Id:number ;
+    FirstName :string;
+    LastName : string;
+    Gender :string ;
+    DateOfBirth : Date;
+    DateOfRegistration : Date;
+    PhoneNumber : string;
+    Email: string;
+    Address1 : string;
+    Address2 : string;
+    City: string;   
+    Zip: string;
+}
