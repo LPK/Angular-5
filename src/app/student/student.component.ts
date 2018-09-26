1f1e68ecd24cb894504d7d2e56be9163681b40bf
LPK <k.lesle@live.com>
2018-09-26 21:44:59 +0530

    test

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { studentservice } from './studentservice';
+import { Student }  from './student';
+
 
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
+  providers :[studentservice],
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+  studentList : Student[];
+  errorMessage : string;
+  constructor(private stdService :studentservice ) { }
 
   ngOnInit() {
+    this.stdService.getStudents().subscribe(
+      emps => this.studentList = emps,
+      error => this .errorMessage = error
+    );
   }
 
 }
